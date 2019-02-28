29e443b1dac32cdeb258e5664906c4f9d2843a15
Martin Barrera <martin.barrrera@outlook.com>
2019-02-28 09:24:45 -0300

    entregable, los links cambiarongit add main.pl funciones.pm

--- funciones.pm
+++ funciones.pm
@@ -4,10 +4,13 @@ use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(secciones votos_por_seccion circuitos_por_secciones votos_por_circuito);
 
-sub trim {
+$basic_url='https://www.justiciacordoba.gob.ar/Estatico/JEL/Escrutinios/ReportesEleccion20150705/';
+
+sub trim { #elimina los espacios al inicio y al final.
  my $string = shift;
- $string =~ s/^\s+//;
- $string =~ s/\s+$//;
+ $string =~ s/^\s+|\s+$//;
+# $string =~ s/^\s+//;
+# $string =~ s/\s+$//;
  return $string;
 }
 
@@ -16,7 +19,7 @@ sub secciones{
   my $html;
   my $secciones_html;
   my %secciones;
-  $html = get 'https://www.justiciacordoba.gob.ar/jel/ReportesEleccion20150705/Index.html'; # pagina principal para obtener las claves
+  $html = get $basic_url.'Index.html'; # pagina principal para obtener las claves
   #$html =~ /(<.*LocalidadesTodos.*<\/select>)/; # filtra el html para obtener las localidades y sus codigos
   $html =~ /(<.*cmbSecciones.*<\/select>)/;
   $secciones_html = $1;
@@ -38,16 +41,13 @@ sub circuitos_por_secciones{
 
   my $html;
 
-  $html = get 'https://www.justiciacordoba.gob.ar/jel/ReportesEleccion20150705/Index.html'; # pagina principal para obtener las claves
+  $html = get $basic_url.'Index.html'; # pagina principal para obtener las claves
   $html =~ s/\n/ /g; #elimina los saltos de linea
   $html =~ /(<script.*<\/script>)/; # filtra la seccion del scrip de donde se van a obtener los datos
   $html = $1;
   $html =~ s/\);/\);\n/g; #agrega saltos de linea al final de las lineas para facilitar el parseo
 
   my %secciones_aux = secciones(); # busca las secciones que existen para asociar los circuitos
-  #foreach my $i (keys %secciones_aux){
-  #  print "$i -> $secciones_aux{$i}\n";
-  #}
   my %secciones;
   foreach my $k (keys %secciones_aux){ #obtendra los circuitos para cada seccion
     $html =~ /arrCircuitosSecc$k.*\((.*)\);\n/; #$k es el numero de seccion al nombre del array es el que esta en el javascript
@@ -85,27 +85,33 @@ sub votos_por_seccion{
   my $html;
   foreach my $code (keys %secciones){ # va a pedir una pagina por cada seccion
     print "$code->$secciones{$code}\n";
-    my $url = "https://www.justiciacordoba.gob.ar/jel/ReportesEleccion20150705/Resultados/E20150705_S".$code."_CA2_0.htm";
+    my $url = $basic_url."Resultados/E20150705_S".$code."_CA2_0.htm";
     #print "$url\n";
     $html = get $url;
     $html =~ s/\n|\r//g; # elimina los saltos de linea
     $html =~ s/<\/TR>\s*/<\/TR>\n/g;# crea separa las filas de la tabla con un salto de linea
     my $aux;
+    my $votos_validos = 0;
+
     foreach my $i(split('\n',$html)){ # filtrara cada linea de la tabla
       foreach my $partido (@partidos){  # comprueba si la linea de tabla concuerda con algun partido
         if($i =~ /(<.*$partido.*)/){
            $aux = $1;
            $aux =~ s/<[^>]*>//g; # elimina los tags html
            $aux =~ s/\s+/ /g; # elimina los espacios multiples
+           print $aux."\n";
            my @line = split(/[\s$partido]+/,$aux); # parsea la linea
            $line[-1] =~ s/,//g; #elimina la ',' de mil que aparece por defecto en el html
            $votos_por_seccion{$code}{$partido} = 0+$line[-1];
            $votos_por_seccion{'TOTAL'}{$partido} += $line[-1];
+           $votos_validos += $line[-1] if(($partido ne 'BLANCOS') and ($partido ne 'NULOS')); # cuenta los votos validos
            #print "@line\n";
            last;
         }
       }
     }
+    $votos_por_seccion{$code}{'VALIDOS'}=$votos_validos;  #carga todos los validos de esa seccion
+    $votos_por_seccion{'TOTAL'}{'VALIDOS'}+=$votos_validos;
     #print "$_ $resultado{$_}\n" for (keys %resultado);
     #print "\n";
   }
@@ -128,27 +134,32 @@ sub votos_por_circuito{
     my %circuitos = %{$circuitos_por_seccion{$sec}};
     foreach my $circ (keys %circuitos){ # va a pedir una pagina por cada circuito
       print "$circ->$circuitos{$circ}\n";
-      my $url = "https://www.justiciacordoba.gob.ar/jel/ReportesEleccion20150705/Resultados/E20150705_C".$circ."_CA2_0.htm";
+      my $url = $basic_url."Resultados/E20150705_C".$circ."_CA2_0.htm";
       #print "$url\n";
       $html = get $url;
       $html =~ s/\n|\r//g; # elimina los saltos de linea
       $html =~ s/<\/TR>\s*/<\/TR>\n/g;# crea separa las filas de la tabla con un salto de linea
       my $aux;
+      my $votos_validos = 0;
       foreach my $i(split('\n',$html)){ # filtrara cada linea de la tabla
         foreach my $partido (@partidos){  # comprueba si la linea de tabla concuerda con algun partido
           if($i =~ /(<.*$partido.*)/){
              $aux = $1;
              $aux =~ s/<[^>]*>//g; # elimina los tags html
              $aux =~ s/\s+/ /g; # elimina los espacios multiples
+             print $aux."\n";
              my @line = split(/[\s$partido]+/,$aux); # parsea la linea
              $line[-1] =~ s/,//g; #elimina la ',' de mil que aparece por defecto en el html
              $votos_por_circuito{$sec}{$circ}{$partido} = 0+$line[-1];
              $votos_por_circuito{'TOTAL'}{'TOTAL'}{$partido} += $line[-1];
+             $votos_validos += $line[-1] if(($partido ne 'BLANCOS') and ($partido ne 'NULOS'));
              #print "@line\n";
              last;
           }
         }
       }
+      $votos_por_circuito{$sec}{$circ}{'VALIDOS'}=$votos_validos;  #carga todos los validos de esa seccion
+      $votos_por_circuito{'TOTAL'}{'TOTAL'}{'VALIDOS'}+=$votos_validos;
     }
   }
   return %votos_por_circuito
--- main.pl
+++ main.pl
@@ -10,7 +10,7 @@ use Excel::Writer::XLSX;
 #============================================================================#
 
 # Create a new Excel workbook
-my $workbook = Excel::Writer::XLSX->new('perl.xlsx');
+my $workbook = Excel::Writer::XLSX->new('perl.xls');
 # Add a worksheet
 $worksheet = $workbook->add_worksheet('por seccion');
 #  Add and define a format
@@ -26,7 +26,10 @@ $format_total->set_align('rigth');
 $format_total->set_bg_color('silver');
 $format_total->set_border_color('black');
 # Write a formatted and unformatted string, row and column notation.
-$col = $row = 0;
+$format_porc = $workbook->add_format(); # Add a format
+$format_porc->set_num_format(0x0a); #formato porcentual
+
+$row = 0;
 
 #=======================================================================#
 #====================   EXTRAE LOS DATOS POR SECCION  ==================#
@@ -45,6 +48,7 @@ my @partidos = ("UNION.POR.CORDOBA",
 
 my %votos_secciones=votos_por_seccion(\%secciones,\@partidos);
 
+ ## escribe los datos en exel
 foreach my $i (sort{$a<=>$b}(keys %votos_secciones)){
   next if $i eq 'TOTAL';
 #  print "\nseccion : $i -> $secciones{$i}\n"; # impresion para consola
@@ -54,18 +58,24 @@ foreach my $i (sort{$a<=>$b}(keys %votos_secciones)){
 #    print "     $j ->  $votos_secciones{$i}{$j}\n"; # impresion del partido y los votos para consola
     $worksheet->write($row,1, "$j"); #escribe el nombre del partido
     $worksheet->write($row,2, "$votos_secciones{$i}{$j}"); # escribe la ctidad de votos que le corresponde en la siguiente celda
+    $worksheet->write($row,3,
+                      $votos_secciones{$i}{$j}/$votos_secciones{$i}{'VALIDOS'},
+                      $format_porc) if $j ne 'BLANCOS' and $j ne 'NULOS' and $j ne 'VALIDOS';
     $row++;
   }
 }
 
 #escribe la seccion de total.
 $row = 1;
-$worksheet->write(0,4, "TOTAL",$format_total); #crea la etiquete para el total
+$worksheet->write(0,5, "TOTAL",$format_total); #crea la etiquete para el total
 print "\nTOTAL:\n";
 foreach my $j (keys %{$votos_secciones{'TOTAL'}}){
   print "     $j ->  $votos_secciones{'TOTAL'}{$j}\n"; # impresion del partido y los votos para consola
-  $worksheet->write($row,4, "$j",$format_total);
-  $worksheet->write($row,5, "$votos_secciones{'TOTAL'}{$j}",$format_total);
+  $worksheet->write($row,5, "$j",$format_total); #nombre del partido
+  $worksheet->write($row,6, "$votos_secciones{'TOTAL'}{$j}",$format_total); #cantidad de votos
+  $worksheet->write($row,7,
+                    $votos_secciones{'TOTAL'}{$j}/$votos_secciones{'TOTAL'}{'VALIDOS'},  #porcentaje de votos
+                    $format_porc) if $j ne 'BLANCOS' and $j ne 'NULOS' and $j ne 'VALIDOS';
   $row++;
 }
 
@@ -94,6 +104,9 @@ for my $sec(sort {$a<=>$b} (keys %resultados_por_circuito)){ #clasifica por circ
       print "            $part   $resultados_por_circuito{$sec}{$circ}{$part}\n";
       $worksheet->write($row,2, "$part"); #escribe el nombre del partido
       $worksheet->write($row,3, "$resultados_por_circuito{$sec}{$circ}{$part}"); # escribe la ctidad de votos que le corresponde en la siguiente celda
+      $worksheet->write($row,4,
+                        $resultados_por_circuito{$sec}{$circ}{$part}/$resultados_por_circuito{$sec}{$circ}{'VALIDOS'},
+                        $format_porc) if $part ne 'BLANCOS' and $part ne 'NULOS' and $part ne 'VALIDOS' and $resultados_por_circuito{$sec}{$circ}{'VALIDOS'} != 0;
       $row++;
     }
   }
@@ -104,8 +117,11 @@ $worksheet->write(0,5, "TOTAL",$format_total); #crea la etiquete para el total
 print "\nTOTAL:\n";
 foreach my $part (keys %{$resultados_por_circuito{'TOTAL'}{'TOTAL'}}){
   print "     $part ->  $resultados_por_circuito{'TOTAL'}{'TOTAL'}{$part}\n"; # impresion del partido y los votos para consola
-  $worksheet->write($row,5, "$part",$format_total);
-  $worksheet->write($row,6, "$resultados_por_circuito{'TOTAL'}{'TOTAL'}{$part}",$format_total);
+  $worksheet->write($row,6, "$part",$format_total);
+  $worksheet->write($row,7, "$resultados_por_circuito{'TOTAL'}{'TOTAL'}{$part}",$format_total);
+  $worksheet->write($row,8,
+                    $resultados_por_circuito{'TOTAL'}{'TOTAL'}{$part}/$resultados_por_circuito{'TOTAL'}{'TOTAL'}{'VALIDOS'},
+                    $format_porc) if $part ne 'BLANCOS' and $part ne 'NULOS' and $part ne 'VALIDOS' and $resultados_por_circuito{'TOTAL'}{'TOTAL'}{'VALIDOS'} != 0;
   $row++;
 }
 
